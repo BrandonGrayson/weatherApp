0f382384fc508ce2d03360645ab419a76683a3bf
Brandon Grayson <brandongrayson@Brandons-Air.fios-router.home>
2020-12-09 20:53:48 -0500

    Added some base css styles

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
+var APIKey = "a8bfa6adc6cea260ba1bbbb01147a568"
+
+  function searchCity(name) {
+
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${APIKey}`
+    var cityInfo
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(response => {
+        console.log('RESPPONSE_________________>', response)
+        cityInfo = response
+        return
+      }).catch(error => console.log(error))
+      return cityInfo
+  }
 $(document).ready(function () {
   // grab the necessary tools to use ajax and weather api
   // create a var to store the queryURL
-  // var for api key to weather app
-  var myName = 'brandon';
-  console.log(myName)
+
+  // SET UP LOCAL STORAGE!!!
   // 1. Create global array to store each city user is searching data for
-  var locationArray = []
-  console.log(locationArray)
   // 2. Once you have an array push each city user is searching into that array(this will be within click event of submit button where you're getting user input)
   // 3. Once you pushed into an array within same click event write localStorage.setItem to set that array into localStorage(make sure to JSON.stringify for an array you're setting up)
   // 4. And then you can retrieve array data back using localstorage.getItem
 
-
-  var APIKey = "a8bfa6adc6cea260ba1bbbb01147a568"
-
+  // var for api key to weather app
   $('#search-city').on('click', searchForCity)
 
   // FUNCTION FOR WHEN SEARCH BUTTON IS PRESSED
+
+  //two functions
+    //on that searchesForCity(name)
+    //oging to add info for the current weather and the 5 day
+
   function searchForCity() {
     console.log('button was clicked')
     // create a button variable 
@@ -24,8 +40,12 @@ $(document).ready(function () {
     // create a var findCity access the textContent of the inputField/previous sibling element
     var findCity = $(this).siblings('#inputField').val()
     // input the url to query insert findCity
-    locationArray.push(findCity)
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${findCity}&appid=${APIKey}`
+
+    var testSearch = searchCity('miami')
+
+    console.log('TEST SEARCH-->', testSearch)
+
     $.ajax({
       url: queryURL,
       method: 'GET'
@@ -34,6 +54,9 @@ $(document).ready(function () {
         //CURRENT WEATHER API CALL
         //FILL IN ALL CURRENT WEATHER HTML
         var name = response.name
+        var searchHistory = JSON.parse(localStorage.getItem('history')) || []
+        searchHistory.push(name)
+        localStorage.setItem('history', JSON.stringify(searchHistory))
         $('#name').text(name)
         //grab lat and lon variables
         const lat = response.coord.lat;
@@ -159,3 +182,31 @@ $(document).ready(function () {
   //$('.input-button').on('click', searchForCity)
   // console.log('INPUT BUTTON CLASS CLICKED')
 })
+
+function loadSearchHistory() {
+  var searchHistory = JSON.parse(window.localStorage.getItem('history')) || [];
+
+  var inputList = $('#input-list')
+
+  for (i = 0; i < searchHistory.length; i++) {
+    var cityName = searchHistory[i]
+    var newLi = $('<li>')
+    newLi.attr('class', 'input-text')
+    newLi.text(cityName)
+    newLi.on('click', function(){
+      // extract code needed to pass in a argument to searchForCity function
+      $.ajax({
+        url: queryURL,
+        method: 'GET'
+      })
+      // access this button that was clicked 
+      // call function
+      // need a function that takes in a name of a city. Access the queryURL / 
+    })
+    inputList.append(newLi)    
+  }
+
+
+
+loadSearchHistory()
+}
